d4dc342b8a0b1c7c81cd4211f7f3466da232f7c9
Salamander <mokmaard646@gmail.com>
2023-01-18 15:44:03 +0700

    update

--- grader/I5/I5_1/I5_1.c
+++ grader/I5/I5_1/I5_1.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
+#include <math.h>
 
 int main () {
-    int size = 5;
-    // ! scanf("%d", &size);
+    int size = 0;
+    scanf("%d", &size);
     int n[size];
-    int x_bar = 0;
-    int _sum = 0;
+    float x_bar = 0;
+    float _sum = 0;
 
     for (int i = 0; i < size; i++) {
         scanf("%d", &n[i]); //size
         x_bar += n[i];
     }
 
-    x_bar /= size;
+    x_bar = x_bar / size;
 
     for (int i = 0; i < size; i++) {
         _sum += pow((n[i] - x_bar), 2);
     } 
 
+    float standard_deviation = pow((_sum) / (size - 1.0), 0.5 );
+
+    printf("%.2f", standard_deviation);
     return 0;
 }
+
+//160 178 169 177 168
