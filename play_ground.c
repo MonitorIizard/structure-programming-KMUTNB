67dc86bb6adb317d5434230936f2c5f0d77714eb
Salamander <mokmaard646@gmail.com>
2023-01-24 20:46:56 +0700

    update

---
+++ play_ground.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main () {
+    int answer = 12 * 11;
+    printf("%d", answer);
+
+    char number[99] = "123456789123456789123456789";
+
+    printf("%d", number);
+
+    answer = 12 * 11;
+    printf("%d", answer);
+    return 0;
+}
