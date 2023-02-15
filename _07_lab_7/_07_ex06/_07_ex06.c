35f14ab8696e36a93f0e5e2eb6f5ededd0f4a776
Salamander <mokmaard646@gmail.com>
2023-02-15 15:05:42 +0700

    update lab 7 ex 6

--- _07_lab_7/_07_ex06/_07_ex06.c
+++ _07_lab_7/_07_ex06/_07_ex06.c
@@ -4,7 +4,7 @@ void drawline(char c, int begin, int end) {
     int i = 0;
 
     for (i = 0; i < end; i++) {
-        if (i < begin) {
+        if (i < begin - 1) {
             printf(" ");
         } else {
             printf("%c",c);
@@ -12,15 +12,42 @@ void drawline(char c, int begin, int end) {
     }
 }
 
-void drawpoint (char c, int *list, int n) {
-
+void drawpoints (char c, int *list, int n) {
+    int _size = list[n-1];
+    int i = 0;
+    int j = 0;
+    
+    for (i = 1; i <= _size; i++) {
+        if (i == list[j]) {
+            printf("%c", c);
+            j++;
+        } else {
+            printf(" ");
+        }
+    }
+    printf("\n");
 }
 
 int main () {
-    drawline('+', 5, 10);
-    int list[10];
+    int list[10]; char c = '*';
+    /* draw triangle */ list[0] = 5; drawpoints(c, list, 1); 
+    list[0] = 3; 
+    list[1] = 7; 
+    drawpoints(c, list, 2); 
+    list[0] = 1; 
+    list[1] = 9;
+    drawpoints(c, list, 2); 
+    drawline(c, 1, 10);
+    /* draw rectangle */ 
+    printf("\n");
+    c = '#';
+    drawline(c, 1, 10); 
+    list[0] = 1; 
+    list[1] = 10;
+    drawpoints(c, list, 2); 
+    drawpoints(c, list, 2); 
+    drawline(c, 1, 10);
+    getchar();
 
-    //draw triangle 
-    
     return 0;
 }
