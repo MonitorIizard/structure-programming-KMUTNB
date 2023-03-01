fd6714afa42c61f98d82daa9e8413ce0ab23688e
Salamander <mokmaard646@gmail.com>
2023-03-01 15:12:30 +0700

    update lab 3

--- grader/l8/l8_3/l8_3.c
+++ grader/l8/l8_3/l8_3.c
@@ -1,15 +1,53 @@
 #include <stdio.h>
 
-void reverse (char *c) {
-    // now *c represent value from that position
-    printf("%s", c);
-}
+// int reverse (char *c) {
+//     // now *c represent value from that position
+//     //int i = 0; j,k;
+
+//     char *p = c;
+//     while (*p != '\0')
+//     {
+//         printf("%c", *p);
+//         p++;
+//     }
+//     return p-c;
+// }
 
 int main () {
-    char messege[999] = "Computer Programming";
+    char messege[999];
     char answer[999];
-    char *m = &messege;
-    printf("%s", &m);
-    //reverse(messege);
+    char *pointer = messege;
+    int i = 0;
+    int size = 0;
+
+    gets(messege);
+    //printf("%s", messege);
+
+    for (int i = 0; i < 100; i++){
+        if(messege[i] == '\0') {
+            break;
+        }
+        size++;
+    }
+
+    int k = 0;
+
+    for (int i = size - 1; i >= 0; i--) {
+        answer[k] = messege[i];
+        printf("%c", answer[k]);
+        k++;
+    }
+    
+    printf("\n");
+
+    int count_number = 0;
+    for (int i = 0; i < size; i++) {
+        if ((int) answer[i] >= 48 && (int) answer[i] <= 57) {
+            count_number++;
+        }
+    }
+    // printf("%d\n", size);
+    // printf("%s\n", answer);
+    printf("%d\n", count_number);
     return 0;
 }
